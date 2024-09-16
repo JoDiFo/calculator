47bb3dc7b2fda6d5759310e995cff05ed9303cad
alexandr-fokin <sasafokin2@gmail.com>
2024-09-16 16:47:28 +0300

    feat: add starting logic to calculator

---
+++ index.js
@@ -0,0 +1,82 @@
+const numberButtons = document.querySelectorAll(".number-btn");
+
+const calculatorDisplay = document.getElementById("display");
+
+const calculateButton = document.getElementById("calculate");
+const resetButton = document.getElementById("reset");
+
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+
+let displayedValue = "0";
+
+const operation = {
+  firstValue: 0,
+  action: "",
+};
+
+numberButtons.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    if (displayedValue === "0") {
+      displayedValue = btn.innerText;
+    } else {
+      displayedValue += btn.innerText;
+    }
+
+    calculatorDisplay.innerText = displayedValue;
+  })
+);
+
+resetButton.addEventListener("click", (e) => {
+  displayedValue = "0";
+  operation.action = "0";
+  operation.firstValue = 0;
+  calculatorDisplay.innerText = displayedValue;
+});
+
+plusButton.addEventListener("click", (e) => {
+  operation.firstValue = Number(displayedValue);
+  operation.action = "+";
+
+  displayedValue = "0";
+
+  calculatorDisplay.innerText = displayedValue;
+});
+
+minusButton.addEventListener("click", (e) => {
+  operation.firstValue = Number(displayedValue);
+  operation.action = "-";
+
+  displayedValue = "0";
+
+  calculatorDisplay.innerText = displayedValue;
+});
+
+multiplyButton.addEventListener("click", (e) => {
+  operation.firstValue = Number(displayedValue);
+  operation.action = "*";
+
+  displayedValue = "0";
+
+  calculatorDisplay.innerText = displayedValue;
+});
+
+divideButton.addEventListener("click", (e) => {
+  operation.firstValue = Number(displayedValue);
+  operation.action = "/";
+
+  displayedValue = "0";
+
+  calculatorDisplay.innerText = displayedValue;
+});
+
+calculateButton.addEventListener("click", () => {
+  displayedValue = eval(
+    `${operation.firstValue}${operation.action}${displayedValue}`
+  );
+  operation.firstValue = Number(displayedValue);
+
+  calculatorDisplay.innerText = displayedValue;
+});
